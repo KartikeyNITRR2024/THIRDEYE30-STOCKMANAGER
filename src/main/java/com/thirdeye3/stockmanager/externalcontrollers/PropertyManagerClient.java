6a51d9de29712685b0f38674e36d1c020245fa99
Kartikey2001nitrr <kartikeythawait123@gmail.com>
2025-09-19 00:55:36 +0530

    updated eureka sequrity

--- src/main/java/com/thirdeye3/stockmanager/externalcontrollers/PropertyManagerClient.java
+++ src/main/java/com/thirdeye3/stockmanager/externalcontrollers/PropertyManagerClient.java
@@ -2,6 +2,7 @@ package com.thirdeye3.stockmanager.externalcontrollers;
 
 import com.thirdeye3.stockmanager.dtos.MachineInfo;
 import com.thirdeye3.stockmanager.dtos.Response;
+import com.thirdeye3.stockmanager.configs.FeignConfig;
 
 import java.util.Map;
 
@@ -9,7 +10,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "THIRDEYE30-PROPERTYMANAGER")
+@FeignClient(
+		name = "THIRDEYE30-PROPERTYMANAGER",
+		configuration = FeignConfig.class
+)
 public interface PropertyManagerClient {
 
     @GetMapping("/pm/machines/webscrapper")
@@ -18,6 +22,6 @@ public interface PropertyManagerClient {
     @GetMapping("/pm/properties")
     Response<Map<String, Object>> getProperties();
 
-    @GetMapping("/api/updateinitiatier/{priority}")
+    @GetMapping("/pm/allserviceinitiatier/{priority}")
     Response<Object> updateInitiatier(@PathVariable("priority") Integer priority);
 }
--- src/main/java/com/thirdeye3/stockmanager/externalcontrollers/SelfClient.java
+++ src/main/java/com/thirdeye3/stockmanager/externalcontrollers/SelfClient.java
@@ -4,10 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.thirdeye3.stockmanager.dtos.Response;
+import com.thirdeye3.stockmanager.configs.FeignConfig;
 
 @FeignClient(
 	    name = "${spring.application.name}", 
-	    url = "${self.url:}"                     
+	    url = "${self.url:}",
+	    configuration = FeignConfig.class
 	)
 	public interface SelfClient {
 
