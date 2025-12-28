0276db5aa6916dbe196d698b4f14979566dcc7cd
Kartikey2001nitrr <kartikeythawait123@gmail.com>
2025-12-28 19:23:08 +0530

    updated apis

--- src/main/java/com/thirdeye3/stockmanager/externalcontrollers/StockViewerClient.java
+++ src/main/java/com/thirdeye3/stockmanager/externalcontrollers/StockViewerClient.java
@@ -10,7 +10,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StockViewerClient {
@@ -25,8 +27,10 @@ public class StockViewerClient {
 
     public Response<List<LatestStockData>> getStockHistory(Long stockId) {
         List<ServiceInstance> instances = discoveryClient.getInstances("THIRDEYE30-STOCKVIEWER");
+        
         ParameterizedTypeReference<Response<List<LatestStockData>>> typeRef = 
                 new ParameterizedTypeReference<>() {};
+
         return Flux.fromIterable(instances)
                 .flatMap(instance -> {
                     String url = instance.getUri().toString() + "/sv/processing/details/" + stockId;
@@ -34,11 +38,21 @@ public class StockViewerClient {
                             .uri(url)
                             .retrieve()
                             .bodyToMono(typeRef)
-                            .filter(res -> res != null && res.isSuccess())
+                            .filter(res -> res != null && res.isSuccess() && res.getResponse() != null)
                             .onErrorResume(e -> Mono.empty()); 
                 })
-                .next() 
-                .switchIfEmpty(Mono.just(new Response<>(false, 404, "Stock history not found on any instance", null)))
+                .collectList() 
+                .map(allResponses -> {
+                    if (allResponses.isEmpty()) {
+                        return new Response<List<LatestStockData>>(false, 404, "Stock history not found on any instance", null);
+                    }
+
+                    List<LatestStockData> mergedAndSortedList = allResponses.stream()
+                            .flatMap(res -> res.getResponse().stream())
+                            .sorted(Comparator.comparing(LatestStockData::getCurrentTime))
+                            .collect(Collectors.toList());
+                    return new Response<List<LatestStockData>>(true, 0, null, mergedAndSortedList);
+                })
                 .block(); 
     }
 }
